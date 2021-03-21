4caded86ac547a9d877556f03c8d45d01e021295
Gyanendra Chaudhary <chaudharygyane699@gmail.com>
2021-03-21 17:22:28 +0545

    writing our first laravel dummy api

---
+++ laravel_apis/app/Http/Controllers/dummyAPI.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class dummyAPI extends Controller
+{
+    //
+    function getData()
+    {
+        return ["name"=>"gyanendra","email"=>"chaudharygyane699@gmail.com",""=>"9825248508"];
+    }
+}
